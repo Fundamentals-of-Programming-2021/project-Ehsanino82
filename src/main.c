0c3d23fd73a49ae610510f22e433c0b69aeda55b
Ehsanino82 <ehsan8202@gmail.com>
2022-02-06 16:20:26 +0330

    soldiers added

--- src/main.c
+++ src/main.c
@@ -15,6 +15,8 @@ struct Soldier{
     int y;
     SDL_Point src;
     SDL_Point dest;
+    SDL_Point dest2;
+    int delay;
     bool is_attacking;
     int id;
 };
@@ -23,6 +25,7 @@ struct Base{
     Sint16 x;
     Sint16 y;
     Uint32 color;
+    Uint32 color2;
     int number_of_soldiers;
     int attacking_soldiers;
     bool is_producing;
@@ -46,11 +49,12 @@ SDL_Texture *getImageTexture(SDL_Renderer *sdlRenderer, char *image_path) {
     return texture;
 }
 
-void generate_random_base(struct Base bases[10], Uint32 colors[4]){
+void generate_random_base(struct Base bases[10], Uint32 colors[5], Uint32 colors2[5]){
     srand(time(0));
     bases[0].x = 60;
     bases[0].y = 60;
     bases[0].color = colors[0];
+    bases[0].color2 = colors2[0];
     bases[0].id = 0;
     bases[0].number_of_soldiers = 0;
     bases[0].attacking_soldiers = 0;
@@ -58,6 +62,7 @@ void generate_random_base(struct Base bases[10], Uint32 colors[4]){
     int i = 1;
     int x, y;
     for(i; i < 5; i++) {
+        flag = 0;
         x = rand() % 610 + 60;
         y = rand() % 430 + 60;
         if((x >= SCREEN_WIDTH / 2 - 50 && x <= SCREEN_WIDTH / 2 + 50) || (y >= SCREEN_HEIGHT / 2 - 30 && y <= SCREEN_HEIGHT / 2 + 50)) {
@@ -74,16 +79,16 @@ void generate_random_base(struct Base bases[10], Uint32 colors[4]){
             bases[i].x = x;
             bases[i].y = y;
             bases[i].color = colors[0];
+            bases[i].color2 = colors2[0];
             bases[i].id = 0;
             bases[i].number_of_soldiers = 0;
             bases[i].attacking_soldiers = 0;
         }else {
             i--;
-            flag = 0;
         }
     }
-    flag = 0;
-    for(i; i < 20; i++){
+    for(i; i < 15; i++){
+        flag = 0;
         int z = 1 + rand()%3;
         x = rand() % 610 + 50;
         y = rand() % 430 + 50;
@@ -101,8 +106,36 @@ void generate_random_base(struct Base bases[10], Uint32 colors[4]){
             bases[i].x = x;
             bases[i].y = y;
             bases[i].color = colors[z];
+            bases[i].color2 = colors2[z];
             bases[i].id = z;
             bases[i].number_of_soldiers = 0;
+            bases[i].attacking_soldiers = 0;
+        }else {
+            i--;
+            flag = 0;
+        }
+    }
+    for(i; i < 20; i++){
+        flag = 0;
+        x = rand() % 610 + 50;
+        y = rand() % 430 + 50;
+        if((x >= SCREEN_WIDTH / 2 - 60 && x <= SCREEN_WIDTH / 2 + 60) || (y >= SCREEN_HEIGHT / 2 - 40 && y <= SCREEN_HEIGHT / 2 + 60)) {
+            i--;
+            continue;
+        }
+        for (int j = 0; j < i; j++) {
+            if (pow(x - bases[j].x,2) + pow(y - bases[j].y,2) > 8000) {
+                flag++;
+                continue;
+            }else
+                break;
+        }if(flag == i) {
+            bases[i].x = x;
+            bases[i].y = y;
+            bases[i].color = colors[4];
+            bases[i].color2 = colors2[4];
+            bases[i].id = 4;
+            bases[i].number_of_soldiers = 30;
             bases[i].attacking_soldiers = 0;
         }else {
             i--;
@@ -113,11 +146,86 @@ void generate_random_base(struct Base bases[10], Uint32 colors[4]){
 
 void draw_bases(SDL_Renderer *sdlRenderer, struct Base base) {
     Sint16 vx[6] = {base.x-30, base.x-15,base.x+15, base.x+30, base.x+15, base.x-15};
-    Sint16 vy[6] = {base.y, base.y-20, base.y-20, base.y, base.y+20, base.y+20};
+    Sint16 vy[6] = {base.y, base.y - (15 * sqrt(3)), base.y - (15 * sqrt(3)), base.y, base.y + (15 * sqrt(3)), base.y + (15 * sqrt(3))};
+    Sint16 vx2[4] = {base.x-10, base.x,base.x+10, base.x};
+    Sint16 vy2[4] = {base.y, base.y - 10, base.y, base.y+10};
     filledPolygonColor(sdlRenderer, vx, vy, 6, base.color);
+    filledPolygonColor(sdlRenderer, vx2, vy2, 4, base.color2);
     char soldiers[3];
     itoa(base.number_of_soldiers, soldiers, 10);
-    stringColor(sdlRenderer, base.x, base.y, soldiers, 0xffaa0000);
+    stringColor(sdlRenderer, base.x -3, base.y + 13, soldiers, 0xffaa0000);
+}
+
+void produce_soldier(struct Base* base){
+    base->number_of_soldiers++;
+}
+
+void attack(struct Base* base){
+    double teta;
+    if(base->soldiers[0].dest.x - base->soldiers[0].src.x != 0)
+        teta = atan(base->soldiers[0].dest.y - base->soldiers[0].src.y) * 1.0 / (base->soldiers[0].dest.x - base->soldiers[0].src.x);
+    for(int i = 0; i < base->number_of_soldiers; i++) {
+        base->soldiers[i].is_attacking = true;
+        base->soldiers[i].delay = (i/5 * 8);
+        base->soldiers[i].x = base->x + ((8 * sin(teta)) * (1 - (i % 5)));
+        base->soldiers[i].y = base->y + ((8 * cos(teta)) * (1 - (i % 5)));
+        base->soldiers[i].dest.x += ((8 * sin(teta)) * (1 - (i % 5)));
+        base->soldiers[i].dest.y += ((8 * cos(teta)) * (1 - (i % 5)));
+    }
+}
+
+void fight(struct Base bases[20], struct Base* base, int n){
+    for(int i = 0; i < 20; i++){
+        if(base->soldiers[n].dest2.x == bases[i].x && base->soldiers[n].dest2.y == bases[i].y) {
+            if (base->id == bases[i].id) {
+                bases[i].number_of_soldiers++;
+            } else {
+                bases[i].number_of_soldiers--;
+                if (bases[i].number_of_soldiers < 0) {
+                    bases[i].number_of_soldiers++;
+                    bases[i].id = base->id;
+                    bases[i].color = base->color;
+                    bases[i].color2 = base->color2;
+                }
+            }
+        }
+    }
+}
+
+void draw_soldiers(SDL_Renderer *sdlRenderer, struct Base* base, struct Base bases[20]){
+    double teta;
+    for(int i = 0; i < base->attacking_soldiers; i++){
+        if(base->soldiers[i].is_attacking) {
+            base->soldiers[i].delay--;
+            if (base->soldiers[i].delay <= -1) {
+                if (base->soldiers[i].dest.x - base->soldiers[i].x != 0) {
+                    teta = atan((base->soldiers[i].dest.y - base->soldiers[i].y) * 1.0 /
+                                (base->soldiers[i].dest.x - base->soldiers[i].x));
+                    if (base->soldiers[i].x > base->soldiers[i].dest.x) {
+                        base->soldiers[i].x -= 3.5 * cos(teta);
+                        base->soldiers[i].y -= 3.5 * sin(teta);
+                    } else {
+                        base->soldiers[i].x += 3.5 * cos(teta);
+                        base->soldiers[i].y += 3.5 * sin(teta);
+                    }
+                } else {
+                    if (base->soldiers[i].y > base->soldiers[i].dest.y)
+                        base->soldiers[i].y -= 3.5;
+                    else
+                        base->soldiers[i].y += 3.5;
+                }
+            } else
+                continue;
+            filledCircleColor(sdlRenderer, base->soldiers[i].x, base->soldiers[i].y, 3, base->color);
+            if (pow(base->soldiers[i].x - base->soldiers[i].dest.x, 2) + pow(base->soldiers[i].y - base->soldiers[i].dest.y, 2) <= 625) {
+                base->soldiers[i].x = base->soldiers[i].dest2.x;
+                base->soldiers[i].y = base->soldiers[i].dest2.y;
+                fight(bases, base, i);
+                base->soldiers[i].is_attacking = false;
+            }
+        }else
+            continue;
+    }
 }
 
 int main() {
@@ -137,16 +245,23 @@ int main() {
     SDL_Texture *sdlTexture = getImageTexture(sdlRenderer, "../loading screen.bmp");
     SDL_Texture *sdlTexture2 = getImageTexture(sdlRenderer, "../input name.bmp");
     SDL_Texture *sdlTexture3 = getImageTexture(sdlRenderer, "../game.bmp");
+    SDL_Texture *sdlTexture4 = getImageTexture(sdlRenderer, "../victory.bmp");
+    SDL_Texture *sdlTexture5 = getImageTexture(sdlRenderer, "../loose.bmp");
     SDL_Rect texture_rect = {.x=0, .y=0, .w=SCREEN_WIDTH, .h=SCREEN_HEIGHT};
     char username[100] = "";
     SDL_StartTextInput();
 
     struct Base bases[20];
-    Uint32 colors[4] = {0xff21218c, 0xff8c6121, 0xff31dbc6, 0xffdbd131};
-
+    Uint32 colors[5] = {0xff0000ff, 0xff00ffff, 0xff00ff00, 0xffffff00, 0xff6e6d6f};
+    Uint32 colors2[5] = {0xff000080, 0xff008080, 0xff008000, 0xff808000, 0xff6e6d6f};
     Sint16 x1 = 100, x2 = 110;
     Sint16 y1 = 350, y2 = 380;
-
+    Sint16 x3 = 390;
+    Sint16 y3 = 280;
+    int t=0;
+    int r=0;
+    int flag = 0;
+    int first = 0;
     SDL_bool shallExit = SDL_FALSE;
     while (shallExit == SDL_FALSE) {
         SDL_SetRenderDrawColor(sdlRenderer, 0xff, 0xff, 0xff, 0xff);
@@ -191,8 +306,15 @@ int main() {
         }
         //menu page end=================================================================================================
         else if(stage == 3){
+            if(t % 50 == 0) {
+                for (int i = 0; i < 20; i++) {
+                    if (bases[i].number_of_soldiers < 30 && bases[i].id != 4)
+                        produce_soldier(&bases[i]);
+                }
+            }
             for(int i = 0; i < 20; i++) {
                 draw_bases(sdlRenderer, bases[i]);
+                draw_soldiers(sdlRenderer, &bases[i], bases);
             }
             // code must be completed to play the game
         }
@@ -235,14 +357,35 @@ int main() {
                     if(500 <= sdlEvent.button.x && 670 >= sdlEvent.button.x && 340 <= sdlEvent.button.y && 370 >= sdlEvent.button.y) {
                         stage = 3;
                         sdlTexture = sdlTexture3;
-                        generate_random_base(bases, colors);
+                        generate_random_base(bases, colors, colors2);
                     }
                     else if(500 <= sdlEvent.button.x && 670 >= sdlEvent.button.x && 380 <= sdlEvent.button.y && 410 >= sdlEvent.button.y)
                         stage = 4;
                     else if(500 <= sdlEvent.button.x && 670 >= sdlEvent.button.x && 420 <= sdlEvent.button.y && 450 >= sdlEvent.button.y)
                         stage = 5;
                 }else if(stage == 3){
-                    // code must be completed to start the game
+                    for(int i = 0; i < 20; i++){
+                        if(bases[i].x-30 <= sdlEvent.button.x && bases[i].x+30 >= sdlEvent.button.x && bases[i].y-25 <= sdlEvent.button.y && bases[i].y+25 >= sdlEvent.button.y){
+                            if(flag == 0 && bases[i].id == 0){
+                                flag++;
+                                first = i;
+                            }else if(flag == 1 && i != first){
+                                for(int j = 0; j < bases[first].number_of_soldiers; j++) {
+                                    bases[first].soldiers[j].src.x = bases[first].x;
+                                    bases[first].soldiers[j].src.y = bases[first].y;
+                                    bases[first].soldiers[j].dest.x = bases[i].x;
+                                    bases[first].soldiers[j].dest.y = bases[i].y;
+                                    bases[first].soldiers[j].dest2.x = bases[i].x;
+                                    bases[first].soldiers[j].dest2.y = bases[i].y;
+                                }
+                                attack(&bases[first]);
+                                bases[first].attacking_soldiers = bases[first].number_of_soldiers;
+                                bases[first].number_of_soldiers = 0;
+                                flag--;
+                                first = 0;
+                            }
+                        }
+                    }
                 }else if(stage == 4){
                     if(500 <= sdlEvent.button.x && 670 >= sdlEvent.button.x && 310 <= sdlEvent.button.y && 340 >= sdlEvent.button.y) {
                         stage = 3;
